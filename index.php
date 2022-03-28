1d2262f11972d4e890be00bc81c1e5829367c8a3
AT180204 <100789741+AT180204@users.noreply.github.com>
2022-03-28 19:07:33 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My profile</title>
+</head>
+<center><body>
+    <h1>Xin chao</h1>
+    <a href="logout.php" title="Logout">Login</a>
+
+    
+</body></center>
+</html>
